0a6cc84f3d42067a700b6556408079d90077c885
RakhaAMCoder <rakaking321@gmail.com>
2025-05-02 01:45:11 +0700

    Update Code

---
+++ Codelab/Modul_4/src/App/Main.java
@@ -0,0 +1,27 @@
+package App;
+
+import Perpustakaan.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Buku nonFiksi = new NonFiksi("Madilog", "Tan Malaka", "Sejarah & Ilmu Pengetahuan");
+        Buku fiksi = new Fiksi("Hainuwele: Sang Putri Kelapa", "Lilis Hu", "Dongeng");
+
+        nonFiksi.displayInfo();
+        fiksi.displayInfo();
+
+        Anggota anggota1 = new Anggota("Rakha Aminullah Muhran", "D128");
+        Anggota anggota2 = new Anggota("Syahid Abdurrahman", "D147");
+
+        anggota1.displayInfo();
+        anggota2.displayInfo();
+
+        anggota1.pinjamBuku("Madilog");
+        anggota2.pinjamBuku("Hainuwele: Sang Putri Kelapa", 7);
+
+        anggota1.kembalikanBuku("Madilog");
+        anggota2.kembalikanBuku("Hainuwele: Sang Putri Kelapa");
+    }
+}
