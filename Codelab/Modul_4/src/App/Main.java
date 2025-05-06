9a6b53eccb1f582d5c5a67ee5c14ba8165c7368a
RakhaAMCoder <rakaking321@gmail.com>
2025-05-06 23:14:50 +0700

    Update Code

--- Codelab/Modul_4/src/App/Main.java
+++ Codelab/Modul_4/src/App/Main.java
@@ -6,11 +6,14 @@ public class Main
 {
     public static void main(String[] args)
     {
+        System.out.println();
         Buku nonFiksi = new NonFiksi("Madilog", "Tan Malaka", "Sejarah & Ilmu Pengetahuan");
         Buku fiksi = new Fiksi("Hainuwele: Sang Putri Kelapa", "Lilis Hu", "Dongeng");
 
         nonFiksi.displayInfo();
         fiksi.displayInfo();
+
+        System.out.println();
 
         Anggota anggota1 = new Anggota("Rakha Aminullah Muhran", "D128");
         Anggota anggota2 = new Anggota("Syahid Abdurrahman", "D147");
@@ -18,8 +21,12 @@ public class Main
         anggota1.displayInfo();
         anggota2.displayInfo();
 
+        System.out.println();
+
         anggota1.pinjamBuku("Madilog");
         anggota2.pinjamBuku("Hainuwele: Sang Putri Kelapa", 7);
+
+        System.out.println();
 
         anggota1.kembalikanBuku("Madilog");
         anggota2.kembalikanBuku("Hainuwele: Sang Putri Kelapa");
